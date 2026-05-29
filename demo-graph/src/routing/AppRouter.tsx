8bf783510b14ba8f3e45823b9deede6209971cc1
Dong Xuan Toan <strangersuntoan@gmail.com>
2026-05-29 22:47:29 +0700

    Add Sliding Windows

--- demo-graph/src/routing/AppRouter.tsx
+++ demo-graph/src/routing/AppRouter.tsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MasterLayout from "../components/MasterLayout";
-import TestBootstrap from "../pages/testbootstrap";
+import Home from "../pages/Home";
 import TestD3 from "../pages/testd3";
 import TestContext from "../pages/testcontext";
 import TestAxios from "../pages/testAxios";
+import NavBar from "../components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MasterLayout />}> 
-          <Route index  element={<TestBootstrap />}/>
+      <NavBar />
+      <div>
+        <Routes>
+          <Route index  element={<Home />}/>
           <Route path='/d3' element={<TestD3 data={[1,2,3]} />}/>
           <Route path='/context' element={<TestContext />}/>
           <Route path='/axios' element={<TestAxios />}/>
-        </Route>
-      </Routes>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
